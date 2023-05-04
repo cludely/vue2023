621418fb1062df17f7668b4bec6d9bf4724ecdc4
cludely <1428448282@qq.com>
2023-05-04 20:09:24 +0800

    🐛解决递归错误的问题

--- src/compile/generate.js
+++ src/compile/generate.js
@@ -62,6 +62,7 @@ function gen(node) {
 
 export function generate(el) {
   let children = genChildren(el)
-  let code = `_c(${el.tag},${el.attrs.length ? `${genProps(el.attrs)}` : 'null'},${children ? `${children}` : 'null'})`
+  let code = `_c('${el.tag}',${el.attrs.length ? `${genProps(el.attrs)}` : 'undefined'}${children ? `,${children}` : ''})`
   console.log(code)
+  return code
 }
